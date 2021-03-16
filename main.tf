a64a5ed994b26da8f885db693e0e741f99951245
BUI Van Tri <vn.tribui@gmail.com>
2021-03-16 14:17:23 +0700

    Update main

--- main.tf
+++ main.tf
@@ -3,10 +3,6 @@
 resource "aws_instance" "this" {
   count         = var.number_of_instances
   instance_type = var.instance_type
-  ami           = "ami-0f86a70488991335e" # ap-southeast-1a
-  key_name      = "trvm"
-  network_interface {
-    network_interface_id = aws_network_interface.tr_ni.id
-    device_index         = 0
-  }
+  ami           = var.ami_instance
+  key_name      = var.key_name
 }   
--- variables.tf
+++ variables.tf
@@ -10,3 +10,14 @@ variable "instance_type" {
   description = "number of instance to create"
   type        = string
 }
+
+variable "ami_instance" {
+  description = "number of instance to create"
+  type        = string
+  default = "ami-0f86a70488991335e"
+}
+
+variable "key_name" {
+  type = string
+  default = "trvm"
+}
