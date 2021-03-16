5528e53dc74e02a93887f5f181a54a46a596bc61
BUI Van Tri <vn.tribui@gmail.com>
2021-03-16 14:11:50 +0700

    Update main

--- main.tf
+++ main.tf
@@ -3,4 +3,10 @@
 resource "aws_instance" "this" {
   count         = var.number_of_instances
   instance_type = var.instance_type
+  ami           = "ami-0f86a70488991335e" # ap-southeast-1a
+  key_name      = "trvm"
+  network_interface {
+    network_interface_id = aws_network_interface.tr_ni.id
+    device_index         = 0
+  }
 }   
